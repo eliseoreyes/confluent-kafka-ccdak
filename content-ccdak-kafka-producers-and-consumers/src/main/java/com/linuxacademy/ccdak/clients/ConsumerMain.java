fe0cd515f16c1c3f126612be7e6e107afec61de4
Eliceo Reyes <300073318@fvtc.edu>
2024-08-11 19:52:01 -0500

    Building a producer

--- content-ccdak-kafka-producers-and-consumers/src/main/java/com/linuxacademy/ccdak/clients/ConsumerMain.java
+++ content-ccdak-kafka-producers-and-consumers/src/main/java/com/linuxacademy/ccdak/clients/ConsumerMain.java
@@ -1,9 +1,40 @@
 package com.linuxacademy.ccdak.clients;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
 public class ConsumerMain {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    	
+	Properties prop = new Properties();
+	prop.setProperty("bootstrap.servers", "localhost:9092");
+	prop.setProperty("group.id", "group1");
+	prop.setProperty("enable.auto.commit", "false");
+	prop.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+	prop.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+	KafkaConsumer<String, String> consumer = new KafkaConsumer<>(prop);
+
+	consumer.subscribe(Arrays.asList("test-topic1", "test-topic2"));
+
+	while(true){
+	   
+	  ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+	  
+	  for (ConsumerRecord<String, String> record : records){
+	  	System.out.println("key=" + record.key() +
+			           ", value=" +record.value() + 
+				   ", topic=" +record.topic() + 
+				   ", partition=" +record.partition() + 
+				   ", offset=" +record.offset());
+	  }
+	   consumer.commitSync();
+	}
     }
 
 }
