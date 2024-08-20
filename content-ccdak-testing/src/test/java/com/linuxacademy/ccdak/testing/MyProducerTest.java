c2d14a2f8f66a847ae37bc0f038825706e405d34
Eliceo Reyes <300073318@fvtc.edu>
2024-08-20 15:41:03 -0500

    Adding kafka testing

---
+++ content-ccdak-testing/src/test/java/com/linuxacademy/ccdak/testing/MyProducerTest.java
@@ -0,0 +1,71 @@
+package com.linuxacademy.ccdak.testing;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+import org.apache.kafka.clients.producer.MockProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author will
+ */
+public class MyProducerTest {
+    
+	MockProducer<Integer, String> mockProducer;
+	MyProducer myProducer;
+
+	private ByteArrayOutputStream systemOutContent;
+	private ByteArrayOutputStream systemErrContent;
+	private final PrintStream originalSystemOut = System.out;
+	private final PrintStream originalSystemErr = System.err;
+
+	@Before
+	public void setUp(){
+		
+		mockProducer = new MockProducer<>(false, new IntegerSerializer(), new StringSerializer());
+		myProducer = new MyProducer();
+		myProducer.producer = mockProducer;
+
+	}
+
+	@Before
+	public void setUpStreams(){
+		
+		systemOutContent = new ByteArrayOutputStream();
+		systemErrContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(systemOutContent));
+		System.setErr(new PrintStream(systemErrContent));
+	}
+
+	@After
+	public void restoreStream(){
+		
+		System.setOut(originalSystemOut);
+		System.setErr(originalSystemErr);
+	}
+
+	@Test
+	public void TestPublishRecord_sent_data(){
+		
+		myProducer.publishRecord(1, "Test Data");
+		mockProducer.completeNext();
+
+		List<ProducerRecord<Integer, String>> records = mockProducer.history();
+
+		Assert.assertEquals(1, records.size());
+		ProducerRecord<Integer,String> record = records.get(0);
+		Assert.assertEquals(Integer.valueOf(1), record.key());
+		Assert.assertEquals("Test Data", record.value());
+		Assert.assertEquals("test_topic", record.topic());
+		Assert.assertEquals("key=1, value=Test Data\n", systemOutContent.toString());
+
+	}
+}
+
